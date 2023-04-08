011b025ee9ab4e201dd5e4d01c76ff36fd7c2cd9
Davi Zappala <94610458+Davizappala@users.noreply.github.com>
2023-04-08 02:09:35 -0300

    add files

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circunferencia
+{
+    internal class calculo
+    {
+
+         public int valor, raio;
+
+        public void Raio(int raio)
+        {
+            this.raio = raio;
+
+           
+        }
+
+        public void circulo(int pelota)
+        {
+            this.valor = pelota;
+        }
+
+        public void pi()
+        {
+            double Calc = 2 * 3.14 * raio;
+
+            Console.WriteLine($"O resultado é: {Calc}");
+            
+        }
+       
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circunferencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculo valor = new calculo();
+
+            Console.WriteLine("Digite o valor do raio");
+
+            valor.Raio(int.Parse(Console.ReadLine()));
+
+            valor.pi();
+        }
+    }
+}
